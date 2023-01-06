07cb8ca66ae06039cbe1d900fede9a9d4628e604
Cezary Sanecki <devcezz@gmail.com>
2023-01-06 12:23:08 +0100

    Initial Ccommit Initial commit

---
+++ src/test/java/pl/cezarysanecki/websocket/WebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
