78a7d41026c40c6b3cd9fdf4cdba65bacba302b5
Cezary Sanecki <devcezz@gmail.com>
2023-01-09 16:52:50 +0100

    Configurated websockets

---
+++ src/main/java/pl/cezarysanecki/websocket/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package pl.cezarysanecki.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topic");
+    registry.setApplicationDestinationPrefixes("/count-app");
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/websocket")
+        .withSockJS()
+        .setHeartbeatTime(100);
+  }
+}
+
