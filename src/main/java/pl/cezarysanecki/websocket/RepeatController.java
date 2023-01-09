a47a4ba051253c8ec6a26114b75cb6302630c4ca
Cezary Sanecki <devcezz@gmail.com>
2023-01-09 16:52:58 +0100

    Added business logic

---
+++ src/main/java/pl/cezarysanecki/websocket/RepeatController.java
@@ -0,0 +1,50 @@
+package pl.cezarysanecki.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Controller
+class RepeatController {
+
+  private static final AtomicInteger COUNTER = new AtomicInteger(1);
+  private static RepeatWord SAVED_WORD_TO_REPEAT = new RepeatWord("default");
+
+  @Autowired
+  private SimpMessagingTemplate simpMessagingTemplate;
+
+  @MessageMapping("/save-word")
+  ResponseEntity<Void> saveWordToRepeat(RepeatWord repeatWord) {
+    assignNewWord(repeatWord);
+    resetCounter();
+    return ResponseEntity.ok().build();
+  }
+
+  @Scheduled(fixedRate = 2000)
+  void repeat() {
+    String content = String.format("Word: %s", SAVED_WORD_TO_REPEAT.value());
+    RepeatResponse response = new RepeatResponse(content, COUNTER.getAndIncrement());
+
+    simpMessagingTemplate.convertAndSend("/topic/repeat", response);
+  }
+
+  private static void assignNewWord(RepeatWord repeatWord) {
+    SAVED_WORD_TO_REPEAT = repeatWord;
+  }
+
+  private static void resetCounter() {
+    COUNTER.set(1);
+  }
+}
+
+record RepeatWord(String value) {
+}
+
+record RepeatResponse(String content, int numberOfRepetitions) {
+}
