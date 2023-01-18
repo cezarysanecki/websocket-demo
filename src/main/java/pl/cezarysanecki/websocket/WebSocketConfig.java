7febec6976445716942585d8f524f11eaadf15e9
Cezary Sanecki <devcezz@gmail.com>
2023-01-18 22:04:36 +0100

    Cleaned code

--- src/main/java/pl/cezarysanecki/websocket/WebSocketConfig.java
+++ src/main/java/pl/cezarysanecki/websocket/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package pl.cezarysanecki.websocket;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,16 +8,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableConfigurationProperties(WebSocketProperties.class)
 @EnableScheduling
 @EnableWebSocketMessageBroker
 class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-  private final WebSocketProperties properties;
-
-  WebSocketConfig(WebSocketProperties properties) {
-    this.properties = properties;
-  }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -29,7 +21,7 @@ class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/websocket")
-        .setAllowedOriginPatterns(properties.getAllowedOrigins())
+        .setAllowedOriginPatterns("*")
         .withSockJS()
         .setHeartbeatTime(1_000);
   }
--- src/test/java/pl/cezarysanecki/websocket/WebSocketDemoApplicationTests.java
+++ src/test/java/pl/cezarysanecki/websocket/WebSocketDemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebsocketDemoApplicationTests {
+class WebSocketDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
